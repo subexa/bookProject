1aecb83b5ee0c3e2d718620c374baf76da072fbf
Aakash Sigdel <aakash.sigdel@gmail.com>
2017-01-08 16:45:55 +0545

    give padding, slice text, button style

--- src/SearchResult.js
+++ src/SearchResult.js
@@ -2,6 +2,14 @@ import React, {Component} from 'react';
 import './SearchResult.css';
 
 class SearchResult extends Component {
+	sliceText = (text) => {
+		if (text.length > 30) {
+			return text.slice(0,30) + "...";
+		} else {
+			return text;
+		}
+	}
+
 	render() {
 		console.log(this.props.myData);
 		let loading = this.props.loadingImage;
@@ -15,8 +23,8 @@ class SearchResult extends Component {
 					<div key={index} className="single-container">
 						<img className="image" />
 						<div className="description">
-							<div className="display-title">{item.title}</div>
-							<div className="display-author-name">{item.author_name[0]}</div>
+							<div className="display-title">{this.sliceText(item.title)}</div>
+							<div className="display-author-name">{this.sliceText(item.author_name[0])}</div>
 						</div>
 					</div>
 				)}
